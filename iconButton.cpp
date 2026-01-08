527f2f3935cbc89380a6f3d9dce94c3fe89d7a46
Jim Lee <github@leftcoast.biz>
2026-01-08 01:58:23 -0800

    Update iconButton.cpp
    Changed to fullClick. touchLift was bleeding into the panel below. fullClick fixed the issue.

--- iconButton.cpp
+++ iconButton.cpp
@@ -2,12 +2,15 @@
 
 
 iconButton::iconButton(int xLoc,int yLoc,const char* path,int pix)
-	: bmpObj(xLoc,yLoc,pix,pix,path) { setEventSet(touchLift); }
+	: bmpObj(xLoc,yLoc,pix,pix,path) { setEventSet(fullClick); }
 	
 	
 iconButton::~iconButton(void) { }
 
 
+// For buttons fullClick is best. If on modals they don't bleed through events to the
+// background. This is written like this so that anything that sets events will trigger
+// the drawing.
 void iconButton::setEventSet(eventSet inEventSet) {
 	
 	if (inEventSet==noEvents) {
@@ -20,10 +23,11 @@ void iconButton::setEventSet(eventSet inEventSet) {
 }
 
 
+// If you know you want this active or not? Use this. It'll do the whole process.
 void iconButton::setActive(bool trueFalse) {
 				
 	if (trueFalse) {
-		setEventSet(touchLift);
+		setEventSet(fullClick);
 	} else {
 		setEventSet(noEvents);
 	}
